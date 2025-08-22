41dfc2c8bcd4913f5dbd13b987ac6a7434258139
Kenzo-UCI <lobokj@uci.edu>
2025-08-22 13:22:59 -0700

    Removed Alert, Displaying List of Cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Alert from "./componenets/Alert";
 import Button from "./componenets/Button";
+import ListGroup from "./componenets/ListGroup/ListGroup";
 import { useState } from "react";
 
 function App() {
   const [showAlert, setShowAlert] = useState(false);
   return (
     <div>
-      {showAlert && <Alert onClose={() => setShowAlert(false)} />}
-      <Button onClick={() => setShowAlert(true)} color="secondary">
-        New Button
-      </Button>
+      <ListGroup
+        items={["New York", "San Francisco", "Tokyo", "London"]}
+        heading="Cities"
+        onSelectItem={(item) => console.log(item)}
+      ></ListGroup>
     </div>
   );
 }
